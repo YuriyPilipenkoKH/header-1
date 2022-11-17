bf6f929048feceabd4c09b20e73f510f017e12f7
Yuriy Pilipenko <yurik2061@gmail.com>
2022-11-17 22:54:57 +0200

    a

---
+++ js/test.js
@@ -0,0 +1,18 @@
+let productName = "Droid";
+let pricePerItem = 2000;
+
+// Change code below this line
+//  productName = "Repair droid";
+//  pricePerItem = 3500;
+
+//  console.log(productName);
+
+ // Change code below this line
+// let topSpeed = 160;
+// let distance = 617.54;
+// let login = "mango935";
+// let isOnLin = true;
+// let isAdmin = false;
+function sayHi () {
+    console.log("Hello, this is my first function!")
+  }
